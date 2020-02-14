11e32328fbd22f1cc55b640058f2680554386b5d
Laura Daugherty <lauradaugherty6@gmail.com>
2020-02-14 10:14:06 -0800

    ex2 comments

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -18,11 +18,15 @@ def reconstruct_trip(tickets, length):
     for i in range (len(tickets)):
       source = tickets[i].source
       destination = tickets[i].destination
+      #insert tickets in to hashtable
       hash_table_insert(hashtable, source, destination)
     
     #find the beginning
     step = hash_table_retrieve(hashtable, "NONE")
+    #while we haven't reached the last ticket
     while step is not "NONE":
+    # add ticket to route
       route.append(step)
+      #move to the next ticket
       step = hash_table_retrieve(hashtable, step)
     return route
